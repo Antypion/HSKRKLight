de4f37c3b7e6336cbf879c3930723b9597f7df81
Krzysztof Kwieciński <k.j.p.kwiecinski@gmail.com>
2014-09-05 22:23:28 +0200

    Initial Commit, created skeletal files

---
+++ app/src/main/java/pl/hskrk/hskrklight/AsyncDownloader.java
@@ -0,0 +1,65 @@
+package pl.hskrk.hskrklight;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Krzysiek on 2014-09-05.
+ */
+public class AsyncDownloader extends AsyncTask<String,Void,String> {
+    String url;
+    @Override
+    protected String doInBackground(String... urls) {
+        url =urls[0];
+        return GET(urls[0]);
+    }
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else{
+                result = "";
+            }
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    // convert inputstream to String
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+}
---
+++ app/src/main/java/pl/hskrk/hskrklight/Light.java
@@ -0,0 +1,50 @@
+package pl.hskrk.hskrklight;
+
+/**
+ * Created by Krzysiek on 2014-09-05.
+ */
+public class Light {
+    private String id;//zawiera nazwę przełącznika(światłe na które oddziałuje)
+    //Contains name of Light bulb that is switched by described toggle
+    private boolean on;//Stan logiczny światła(włączone/wyłączone)
+    //Tells you if light is on or off
+    //Konstruktor/Constructor
+    public Light(String id){
+        this.id = id;
+        on = false;
+    }
+    public Light(String id,boolean on){
+        this.id = id;
+        this.on = on;
+    }
+    //Przęłącznie światła
+    //Toggling the light
+    public void toggle(){
+        if(on){
+            //Switch off
+            on = false;
+        }else{
+            //Switch on
+            on = true;
+        }
+        //Tutaj się walnie obługę wyjątków
+    }
+    public String hasBeenLit(){
+        if(on) return("on");
+        else return("off");
+    }
+    public boolean getState(){
+        return on;
+    }
+    public void setState(boolean has_been_lit){
+        on = has_been_lit;
+    }
+    public String getName(){
+        return id;
+    }
+
+    public String toString(){
+        return(id +'('+this.hasBeenLit()+')');
+        //return id;
+    }
+}
